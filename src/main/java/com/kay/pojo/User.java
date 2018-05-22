06b3e8e5bff5f4b17efa8e982122891dfb6dc773
LiuKay <passionno1@qq.com>
2018-05-22 10:46:06 +0800

    Spring Session 注入实现无侵入的单点登录

--- src/main/java/com/kay/pojo/User.java
+++ src/main/java/com/kay/pojo/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
 
 
@@ -12,7 +13,7 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable{
     private Integer id;
 
     private String username;
