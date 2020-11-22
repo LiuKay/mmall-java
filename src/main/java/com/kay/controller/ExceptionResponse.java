df9a6d0b43175965e3b39eae24f516f009011c62
LiuKay <kayfen@foxmail.com>
2020-11-22 16:30:55 +0800

    refactor code

---
+++ src/main/java/com/kay/controller/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package com.kay.controller;
+
+import lombok.Data;
+
+/**
+ * TODO: refactor
+ */
+@Data
+public class ExceptionResponse {
+    private String message;
+    private String details;
+
+}
