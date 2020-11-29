aebaf853e2bc5fd0626e0847b4473269413bfd1e
LiuKay <kayfen@foxmail.com>
2020-11-29 16:27:26 +0800

    refactor exception and error handler

--- src/main/java/com/kay/controller/LoginController.java
+++ src/main/java/com/kay/controller/LoginController.java
@@ -1,11 +1,17 @@
 package com.kay.controller;
 
+import com.kay.common.ApiErrorResponse;
+import com.kay.security.properties.SecurityConstants;
 import com.kay.service.AuthService;
+import com.kay.util.TimestampProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,9 +26,12 @@ public class LoginController {
 
     private final AuthService authService;
 
+    private final TimestampProvider timestampProvider;
+
     @Autowired
-    public LoginController(AuthService authService) {
+    public LoginController(AuthService authService, TimestampProvider timestampProvider) {
         this.authService = authService;
+        this.timestampProvider = timestampProvider;
     }
 
     @GetMapping("/code/smsCode")
@@ -31,4 +40,16 @@ public class LoginController {
         authService.sendVerificationCode(mobile);
     }
 
+
+    @GetMapping(SecurityConstants.LOGIN_REQUIRE)
+    public ApiErrorResponse requireLogin(HttpServletRequest request) {
+        return ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .errorCode(HttpStatus.UNAUTHORIZED.toString())
+                .message("Need Login.")
+                .detail("Require authentication, please login first.")
+                .timestamp(timestampProvider.getTimestampAsString())
+                .path(request.getRequestURI())
+                .build();
+    }
 }
