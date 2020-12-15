e7f646e2a967de0f37d7035b553ae5532e545e7b
LiuKay <passionno1@qq.com>
2020-12-15 21:24:45 +0800

    upgrade jsonwebtoken lib

--- src/main/java/com/kay/MallApplication.java
+++ src/main/java/com/kay/MallApplication.java
@@ -4,10 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author kay
- * @date 2019/8/23 21:50
- */
 @SpringBootApplication
 @MapperScan("com.kay.dao")
 public class MallApplication {
