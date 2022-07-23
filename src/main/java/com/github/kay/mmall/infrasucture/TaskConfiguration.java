b5f5cd534209897de849d786cd7611f41e1324e6
LiuKay <kayfen@foxmail.com>
2022-07-23 20:10:16 +0800

    chore: remove useless classes

--- src/main/java/com/github/kay/mmall/infrasucture/TaskConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.kay.mmall.infrasucture;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@EnableScheduling
-@Configuration
-public class TaskConfiguration {
-}
