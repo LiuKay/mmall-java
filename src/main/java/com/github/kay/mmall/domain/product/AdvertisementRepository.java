e26989e34acb80e1f6a56d320fc7a957af73df2d
kaybee <kayfen@foxmail.com>
2022-07-27 18:48:18 +0800

    chore: update resources

--- src/main/java/com/github/kay/mmall/domain/product/AdvertisementRepository.java
+++ src/main/java/com/github/kay/mmall/domain/product/AdvertisementRepository.java
@@ -1,8 +1,6 @@
 package com.github.kay.mmall.domain.product;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AdvertisementRepository extends CrudRepository<Advertisement, Integer> {
-    Iterable<Advertisement> findAll() throws DataAccessException;
 }
