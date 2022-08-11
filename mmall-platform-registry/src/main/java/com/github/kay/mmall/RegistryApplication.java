6c4e147e7533780b489506d6acdd3dfee9b4991e
kaybee <kayfen@foxmail.com>
2022-08-11 20:36:50 +0800

    feat: add gateway, registry, config server

---
+++ mmall-platform-registry/src/main/java/com/github/kay/mmall/RegistryApplication.java
@@ -0,0 +1,13 @@
+package com.github.kay.mmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
